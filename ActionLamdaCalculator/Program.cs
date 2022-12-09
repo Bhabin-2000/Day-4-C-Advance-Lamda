4425b1ce651235fdf63b0b4ae88b114bfa87fe61
Bhabin-2000 <119649122+Bhabin-2000@users.noreply.github.com>
2022-12-09 12:00:52 +0530

    Add files via upload

---
+++ ActionLamdaCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ActionLamdaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Action Lamda Calculator");
+
+            Action<int, int> addNumber = (firstNumber, secondNumber) =>
+            Console.WriteLine($"Adding Number is : {firstNumber + secondNumber}");
+
+            Action<int, int> subractNumber = (firstNumber, secondNumber) =>
+              Console.WriteLine($"Subracting Number is : {firstNumber - secondNumber}");
+
+            Action<int, int> multipleNumber = (firstNumber, secondNumber) =>
+            Console.WriteLine($"Adding Number is : {firstNumber * secondNumber}");
+
+            Action<int, int> divisiontNumber = (firstNumber, secondNumber) =>
+            Console.WriteLine($"Subracting Number is : {firstNumber / secondNumber}");
+
+            Calculator(addNumber += subractNumber += multipleNumber += divisiontNumber, 12, 4);
+
+            Console.ReadKey();
+
+
+
+            static void Calculator(Action<int, int> values, int firstNumber, int secondNumber)
+            {
+                values(firstNumber, secondNumber);
+            }
+
+        }
+    }
+}
---
+++ FuncLamdaCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FuncLamdaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Func Lamda Calculator  ");
+
+            Func<int, int, int> addnumber =  (firstNum, secondNumber) => firstNum + secondNumber;
+
+            Func<int, int, int> subractnumber = ( firstNum,  secondNumber) => firstNum - secondNumber;
+
+
+            Func<int, int, int> multiplenumber = ( firstNum,  secondNumber) => firstNum * secondNumber;
+
+            Func<int, int, int> divisionnumber = ( firstNum,  secondNumber) => firstNum / secondNumber;
+
+
+            Calculator(addnumber, 16, 4);
+            Calculator(subractnumber, 16, 4);            
+            Calculator(multiplenumber, 16, 4);
+            Calculator(divisionnumber, 16, 4);
+
+            static void Calculator(Func<int, int, int> values, int firstNum, int secondNumber)
+            {
+                Console.WriteLine(values(firstNum, secondNumber));
+            }
+
+        }
+    }
+}
